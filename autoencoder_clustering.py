ba81f946ccfaf52285b569d2e5ca521f71814e5e
Elod Csirmaz <csirmazelodpal@gmail.com>
2020-07-03 17:43:13 +0100

    Combine force towards vertices with quantisation

--- autoencoder_clustering.py
+++ autoencoder_clustering.py
@@ -3,12 +3,14 @@ from keras import backend as K
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 config = {
     'inputs': 28*28,
-    'layer_sizes': [600, 500, 400, 300, 200, 150, 100, 75, 50, 30, 20, 10, 5],
+    'layer_sizes': [600, 500, 400, 300, 200, 150, 100, 75, 50, 30, 20, 10, 4],
     'batch_size': 64,
-    'epochs': 200,
+    'bottleneck_cells': 20., # TODO 8.,
+    'epochs': 100,
     'label_num': 10, # How many labels there are in the data
     'samples': 20000 # How many data points to predict on in the evaluation phase (unless the test set is used)
 }
@@ -18,8 +20,14 @@ config = {
 
 def bottleneck_loss(y_true, y_pred):
     # y_true_prot = tf.stop_gradient(y_true)
+    
     loss = 1. - 1./( 1. + tf.math.exp(-25.*((y_pred-.5)**2.)) )
     loss *= 2.
+    
+    # loss = 1. - tf.math.cos(y_pred * math.pi * .8)
+    
+    # loss = 0.
+    
     return tf.reduce_mean(loss)
 
 
@@ -36,7 +44,7 @@ for ix, size in enumerate(config['layer_sizes']):
 x = keras.layers.Activation('sigmoid')(x) # to ensure bottleneck signal is 0-1
 
 bottleneck = x
-quantised_bottleneck = keras.layers.Lambda((lambda y: tf.math.floor(y*1.9999)))(x)
+quantised_bottleneck = keras.layers.Lambda((lambda y: tf.math.floor(y*(config['bottleneck_cells']-.0001))/config['bottleneck_cells'] ))(x)
 
 decoder_input = keras.layers.Input(shape=(config['layer_sizes'][-1],), dtype='float32')
 dx = decoder_input
@@ -58,7 +66,7 @@ quantised_decoded = keras.layers.Lambda((lambda y: y), name='quantised_decoded')
 model = keras.models.Model(inputs=[inputs], outputs=[decoded, bottleneck, quantised_decoded])
 model.compile(
     loss={'decoded': 'mse', 'bottleneck': bottleneck_loss, 'quantised_decoded': 'mse'},
-    loss_weights={'decoded': 1., 'bottleneck': .02, 'quantised_decoded': .5},
+    loss_weights={'decoded': 1., 'bottleneck': .01, 'quantised_decoded': 2.},
     optimizer=keras.optimizers.Adadelta()
 )
 
@@ -101,7 +109,7 @@ for i in range(bottleneck_dimensions):
     for j in range(i+1, bottleneck_dimensions):
         plt.scatter(code[:,i], code[:,j], s=.4, c=label_index)
         plt.title("Bottleneck scatter Dimensions i={} j={}".format(i, j))
-        plt.savefig("out/bottleneck_scatter_{}_{}.png".format(i, j), dpi=200)
+        plt.savefig("out/bottleneck_scatter_4fq20_{}_{}.png".format(i, j), dpi=200)
 
 # Collect stats on what kind of data points are in the clusters (corners)
 
@@ -137,4 +145,4 @@ for cluster in range(2**bottleneck_dimensions):
             input[0, i] = 1
     decoded = decoder_model.predict(input)[0]
     decoded = np.reshape(decoded, (28, 28))
-    plt.imsave("out/decoded_{}.png".format(cluster), decoded)
+    plt.imsave("out/decoded_4fq20_{}.png".format(cluster), decoded)
